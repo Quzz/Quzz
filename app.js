32c791d0433f8d5b9de53c56a6149471ed4dc620
Jan-Henrik <jh.bruhn97@gmail.com>
2013-11-04 19:20:14 +0100

    using app.config

--- app.js
+++ app.js
@@ -22,32 +22,31 @@ var sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
 var Hashids = require("hashids"),
     hashids = new Hashids("Erdbeermarmelade!");
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.configure(function() {
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(cookieParser);
-app.use(express.session({store: sessionStore}));
-app.use(app.router);
-app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(cookieParser);
+  app.use(express.session({store: sessionStore}));
+  app.use(app.router);
+  app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
 
-// development only
-if ('development' == app.get('env')) {
+app.configure('development', function() {
+  // development only
   app.use(express.errorHandler());
-}
-
-
+});
 
 app.get('/', routes.index);
 app.get('/client/:id', routes.client);
-
 
 sessionSockets.on('connection', function (err, socket, session) {
   if(!session) {
